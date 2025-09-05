e4f5354639ecce9eba0277e9245beb6a3f60ab76
Antonio Espinoza <aespinozareyes9@gmail.com>
2025-09-05 10:32:51 -0400

    add insertData.sql for initial student data and update test to reflect new data

---
+++ target/classes/insertData.sql
@@ -0,0 +1,7 @@
+insert into student (id, firstname, lastname, email_address) values (10, 'John', 'Doe', 'john@gmail.com')
+
+insert into student (id, firstname, lastname, email_address) values (20, 'Jane', 'Smith', 'Jane@gmail.com')
+
+insert into student (id, firstname, lastname, email_address) values (30, 'Michael', 'Johnson', 'j@gmail.com')
+
+insert into student (id, firstname, lastname, email_address) values (40, 'Emily', 'Davis', 'emi@gmail.com')
